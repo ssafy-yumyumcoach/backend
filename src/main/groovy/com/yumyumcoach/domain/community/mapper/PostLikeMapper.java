2d38cec4af2c707bdb8bf5340d6915d1595990b0
BEAR <ungnam0509@gmail.com>
2025-12-10 13:40:01 +0900

    feat(#11): postLike mapper, entity 작성

--- src/main/groovy/com/yumyumcoach/domain/community/mapper/PostLikeMapper.java
+++ src/main/groovy/com/yumyumcoach/domain/community/mapper/PostLikeMapper.java
@@ -1,4 +1,28 @@
 package com.yumyumcoach.domain.community.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface PostLikeMapper {
+    /**
+     * 특정 유저가 특정 게시글에 좋아요 눌렀는지 여부
+     */
+    boolean existsByPostIdAndAuthorEmail(@Param("postId") Long postId,
+                                         @Param("authorEmail") String authorEmail);
+
+    /**
+     * 좋아요 추가
+     */
+    void insert(PostLike like);
+
+    /**
+     * 좋아요 취소
+     */
+    void deleteByPostIdAndAuthorEmail(@Param("postId") Long postId,
+                                      @Param("authorEmail") String authorEmail);
+
+    /**
+     * 특정 게시글의 좋아요 개수
+     */
+    long countByPostId(@Param("postId") Long postId);
 }
