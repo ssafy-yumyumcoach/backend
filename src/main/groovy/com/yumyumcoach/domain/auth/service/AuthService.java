eb637ce1b55fb53428e8123e64a80d9e497e8bb3
BEAR <ungnam0509@gmail.com>
2025-12-15 01:00:02 +0900

    refactor(#36): auth 예외처리 표준화

--- src/main/groovy/com/yumyumcoach/domain/auth/service/AuthService.java
+++ src/main/groovy/com/yumyumcoach/domain/auth/service/AuthService.java
@@ -5,7 +5,8 @@ import com.yumyumcoach.domain.auth.dto.LoginResponse;
 import com.yumyumcoach.domain.auth.dto.UserInfo;
 import com.yumyumcoach.domain.auth.entity.Account;
 import com.yumyumcoach.domain.auth.mapper.AccountMapper;
-import com.yumyumcoach.global.exception.InvalidCredentialsException;
+import com.yumyumcoach.global.exception.BusinessException;
+import com.yumyumcoach.global.exception.ErrorCode;
 import com.yumyumcoach.global.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,7 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 성공 시 JWT 방식의 access token 과 refresh token 생성 후
 
 성공 시: 로그인 정보 반환
-실패 시: InvalidCredentialsException 예외 던짐
+실패 시: BusinessException 예외 던짐
  */
 
 @Service
@@ -34,11 +35,11 @@ public class AuthService {
 
         // 해당 이메일이 DB 에 없을 때
         if (account == null) {
-            throw new InvalidCredentialsException("등록되지 않은 회원입니다.");
+            throw new BusinessException(ErrorCode.AUTH_INVALID_CREDENTIALS, "등록되지 않은 회원입니다.");
         }
         // 이메일은 DB 에 존재하나 비밀번호가 틀렸을 때
         else if(!passwordEncoder.matches(request.getPassword(), account.getPassword())) {
-            throw new InvalidCredentialsException("비밀번호가 일치하지 않습니다.");
+            throw new BusinessException(ErrorCode.AUTH_INVALID_CREDENTIALS, "비밀번호가 일치하지 않습니다.");
         }
 
         //access token 생성, refresh token 생성
