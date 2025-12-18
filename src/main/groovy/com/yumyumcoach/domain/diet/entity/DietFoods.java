54c9d0b2d8aab03002398936dd5f09b1f2888805
gyubin <usb196057@hotmail.com>
2025-12-18 18:49:06 +0900

    feat(#34): jpa 어노테이션 및 import 삭제

--- src/main/groovy/com/yumyumcoach/domain/diet/entity/DietFoods.java
+++ src/main/groovy/com/yumyumcoach/domain/diet/entity/DietFoods.java
@@ -1,27 +1,15 @@
 package com.yumyumcoach.domain.diet.entity;
 
-import jakarta.persistence.*;
-
-@Entity
-@Table(name="diet_foods")
 public class DietFoods {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @ManyToOne(fetch = FetchType.LAZY, optional = false)
-  @JoinColumn(name = "diet_id", nullable = false, unique = true) // 어느 식단에 해당하는 음식인지
   private DietRecords dietId;
   
-  @Column(name = "order_index", nullable = false, unique = true) // 식단에 등록되는 음식의 순서
   private Integer orderIndex;
   
-  @ManyToOne(fetch = FetchType.LAZY, optional = false)
-  @JoinColumn(name = "food_id", nullable = false) // 하나의 식단에 여러 음식이 있을 수 있음
   private Foods foodId;
   
-  @Column(name = "weight", nullable = true)
   private Double weight;
   
   protected DietFoods() {}
